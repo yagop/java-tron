6af02f345c8d894ec4526a0f73b8ba2fadded25f
halibobo1205 <halibobo1205@gmail.com>
2021-08-11 16:48:05 +0800

    pre-merge for DynamicPropertiesStore

--- chainbase/src/main/java/org/tron/core/db2/core/SnapshotImpl.java
+++ chainbase/src/main/java/org/tron/core/db2/core/SnapshotImpl.java
@@ -24,12 +24,21 @@ public class SnapshotImpl extends AbstractSnapshot<Key, Value> {
 
   SnapshotImpl(Snapshot snapshot) {
     synchronized (this) {
-      db = new HashDB(SnapshotImpl.class.getSimpleName());
+      db = new HashDB(SnapshotImpl.class.getSimpleName() + ":" + root.getDbName());
     }
 
     root = snapshot.getRoot();
     previous = snapshot;
     snapshot.setNext(this);
+
+    // merge for DynamicPropertiesStore，100 keys
+    if ("properties".equalsIgnoreCase(root.getDbName())) {
+      if (root == previous ){
+        Streams.stream(root.iterator()).forEach( e -> put(e.getKey(),e.getValue()));
+      }else {
+        merge(previous);
+      }
+    }
   }
 
   @Override
@@ -40,6 +49,12 @@ public class SnapshotImpl extends AbstractSnapshot<Key, Value> {
   private byte[] get(Snapshot head, byte[] key) {
     Snapshot snapshot = head;
     Value value;
+
+    if ("properties".equalsIgnoreCase(root.getDbName())) {
+      value = db.get(Key.of(key));
+      return value == null ? null: value.getBytes();
+    }
+
     while (Snapshot.isImpl(snapshot)) {
       if ((value = ((SnapshotImpl) snapshot).db.get(Key.of(key))) != null) {
         return value.getBytes();
