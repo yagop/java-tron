71de19d20879c722dfb3ecdae6ba787376b3dacf
liqi <1242201319@qq.com>
2021-10-15 15:29:31 +0800

    modify miner

--- framework/src/test/java/stest/tron/wallet/dailybuild/jsonrpc/Accounts001.java
+++ framework/src/test/java/stest/tron/wallet/dailybuild/jsonrpc/Accounts001.java
@@ -153,6 +153,7 @@ public class Accounts001 extends JsonRpcBase {
     params.add(param);
     JsonObject requestBody = getJsonRpcBody("eth_estimateGas", params);
     response = getJsonRpc(jsonRpcNode, requestBody);
+    logger.info("test06requestBody:" + requestBody);
     responseContent = HttpMethed.parseResponseContent(response);
     String dataResult = responseContent.getString("result");
     Assert.assertEquals("0x147", dataResult);
@@ -1009,7 +1010,7 @@ public class Accounts001 extends JsonRpcBase {
     Assert.assertEquals(getBlockByHashResult.getString("hash"), "0x" + bid);
     Assert.assertEquals(getBlockByHashResult.getString("parentHash"), "0x" + parentHash);
     Assert.assertEquals(getBlockByHashResult.getString("transactionsRoot"), "0x" + txTrieRoot);
-    Assert.assertEquals(getBlockByHashResult.getString("miner"), "0x" + witnessAddress);
+    Assert.assertEquals(getBlockByHashResult.getString("miner"), "0x" + witnessAddress.substring(2));
     Assert.assertEquals(getBlockByHashResult.getString("gasUsed"), "0x" + Long.toHexString(gas));
     Assert.assertEquals(
         String.valueOf(Long.parseLong(getBlockByHashResult.getString("gasLimit").substring(2), 16)),
@@ -1078,7 +1079,7 @@ public class Accounts001 extends JsonRpcBase {
     Assert.assertEquals(getBlockByNumberResult.getString("hash"), "0x" + bid);
     Assert.assertEquals(getBlockByNumberResult.getString("parentHash"), "0x" + parentHash);
     Assert.assertEquals(getBlockByNumberResult.getString("transactionsRoot"), "0x" + txTrieRoot);
-    Assert.assertEquals(getBlockByNumberResult.getString("miner"), "0x" + witnessAddress);
+    Assert.assertEquals(getBlockByNumberResult.getString("miner"), "0x" + witnessAddress.substring(2));
     Assert.assertEquals(getBlockByNumberResult.getString("gasUsed"), "0x" + Long.toHexString(gas));
     Assert.assertEquals(
         String.valueOf(
